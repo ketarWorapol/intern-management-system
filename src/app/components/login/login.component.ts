d96514c5a5904e2e585d5489578f748828fc7d04
tct48 <tar_solo@outlook.co.th>
2020-06-15 19:21:40 +0700

    commit before add angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor(
+    private builder: FormBuilder,
+    
   ) { }
 
   ngOnInit() {
